e387445ed656a4bd32a23e40c5cb168c0da65e00
Erica Calaça <ericazpc@gmail.com>
2023-11-02 14:31:24 -0700

    Created a menu with a drowpdown menu to add more students to the list. Also, created buttons to delete and edit students

--- StudentPractice/src/main/java/com/example/studentpractice/StudentPracticeApplication.java
+++ StudentPractice/src/main/java/com/example/studentpractice/StudentPracticeApplication.java
@@ -16,17 +16,17 @@ public class StudentPracticeApplication {
         SpringApplication.run(StudentPracticeApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
-        return args -> {
-            studentRepository.save(new Student(null, "Jam", new Date(), false, 1.2));
-            studentRepository.save(new Student(null, "Jen", new Date(), true, 3.2));
-            studentRepository.save(new Student(null, "Jok", new Date(), false, 1.0));
-            studentRepository.save(new Student(null, "Jos", new Date(), false, 4.2));
-            studentRepository.findAll().forEach(p->{
-                System.out.println(p.getName());
-            });
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+//        return args -> {
+//            studentRepository.save(new Student(null, "Jam", new Date(), false, 1.2));
+//            studentRepository.save(new Student(null, "Jen", new Date(), true, 3.2));
+//            studentRepository.save(new Student(null, "Jok", new Date(), false, 1.0));
+//            studentRepository.save(new Student(null, "Jos", new Date(), false, 4.2));
+//            studentRepository.findAll().forEach(p->{
+//                System.out.println(p.getName());
+//            });
+//        };
+//    }
 
 }
